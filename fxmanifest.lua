87c317a648e0b21edfa7c444c2d39971f5061b16
thomappp <143356129+thomappp@users.noreply.github.com>
2023-09-24 16:46:15 +0200

    Add files via upload

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'bodacious'
 game 'gta5'
 
 author 'Thomapp'
-description 'Weather Menu Script.'
+description 'Dynamic Weather Menu Script.'
 
 client_scripts { 'client/cl_main.lua', 'client/cl_menu.lua' }
 server_scripts { 'server/sv_main.lua', 'server/sv_config.lua' }
